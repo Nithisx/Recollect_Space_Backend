a5bbf2b4b96dd59e85f0d5b1024700709aba0cc5
Nithisx <nithishkumarnk182005@gmail.com>
2024-12-31 11:54:10 +0530

    update environment variables for MongoDB URI and server port configuration

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use('/api', blogRoutes);
 
 
 
-const PORT = process.env.PORT || 5003; 
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
+app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
+    console.log(`Server is running on port ${process.env.PORT || 3000}`);
+  });
+  
